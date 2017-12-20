5be655cbf248d2db388a851465f0493eb58d8d00
Rob Fletcher <rfletcher@netflix.com>
2017-12-20 15:55:15 -0800

    fixed merge conflicts after rebasing

--- dev/path.zsh
+++ dev/path.zsh
@@ -1,2 +1,9 @@
 export PATH=$PATH:$HOME/.cargo/bin
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+export GOPATH=~/Projects/go
+export PATH=$PATH:$GOPATH/bin
+
+export JAVA_HOME=`/usr/libexec/java_home`
+export GROOVY_HOME=/usr/local/opt/groovy/libexec
+export PATH=$GROOVY_HOME/bin:$PATH
