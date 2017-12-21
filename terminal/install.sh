09298a354b4f14784ab1112da01795f707b8819a
Rob Fletcher <rfletcher@netflix.com>
2017-12-21 15:17:24 -0800

    install fonts using cask

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 defaults write -app iTerm 'Custom Color Presets' -dict-add "Material" "$(cat ~/.material-iterm/material.itermcolors)"
 defaults write -app iTerm 'Custom Color Presets' -dict-add "Material Dark" "$(cat ~/.material-iterm/material-dark.itermcolors)"
+
+brew tap caskroom/fonts
