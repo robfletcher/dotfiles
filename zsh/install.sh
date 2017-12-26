e14efbfc74b0d932b35ed74d1a44f7ffd0926e35
Rob Fletcher <rfletcher@netflix.com>
2017-12-25 18:08:30 -0800

    DRYer script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DIR=~/.oh-my-zsh/custom/themes/powerlevel9k
 if [[ ! -d $DIR ]]; then
-  mkdir -p ~/.oh-my-zsh/custom/themes/
-  ln -s ~/.powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
+  mkdir -p `dirname $DIR`
+  ln -s ~/.powerlevel9k $DIR
 fi
