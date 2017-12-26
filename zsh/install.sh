583059001d0ec32bd0d84dc194874d7f08dcd086
Rob Fletcher <rfletcher@netflix.com>
2017-12-25 17:57:39 -0800

    fix error when symlink already exists

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-mkdir -p ~/.oh-my-zsh/custom/themes/
-ln -s ~/.powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
+DIR=~/.oh-my-zsh/custom/themes/powerlevel9k
+if [[ ! -d $DIR ]]; then
+  mkdir -p ~/.oh-my-zsh/custom/themes/
+  ln -s ~/.powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
+fi
