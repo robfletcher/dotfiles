1657c94e1e5f0e700b7e363a6baf87199d84cdb2
Rob Fletcher <rfletcher@netflix.com>
2018-07-28 19:08:50 -0700

    fix karabiner install script

--- keyboard/install.sh
+++ keyboard/install.sh
@@ -2,5 +2,6 @@
 DIR=~/.config/karabiner
 if [[ ! -d $DIR ]]; then
   mkdir -p `dirname $DIR`
-  cp keyboard/karabiner.json $DIR
+  SCRIPTDIR=$(exec 2>/dev/null;cd -- $(dirname "$0"); unset PWD; pwd)
+  cp -f $SCRIPTDIR/karabiner.json $DIR
 fi
