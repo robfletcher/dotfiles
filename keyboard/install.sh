b3abf6a7772d5ab8a2ca378d3c45bc6ad747911c
Rob Fletcher <rfletcher@netflix.com>
2018-07-30 16:58:34 -0700

    neater way to determine script path

--- keyboard/install.sh
+++ keyboard/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-SCRIPTDIR=$(dirname `realpath "$0"`)
-DIR=~/.config/karabiner
-if [[ ! -d $DIR ]]; then
-  mkdir -p $DIR
-  cp -f $SCRIPTDIR/karabiner.json $DIR/karabiner.json
+SOURCEDIR=$(dirname `greadlink -fn "$0"`)
+TARGETDIR=~/.config/karabiner
+if [[ ! -d $TARGETDIR ]]; then
+  mkdir -p $TARGETDIR
+  cp $SOURCEDIR/karabiner.json $TARGETDIR/karabiner.json
 fi
