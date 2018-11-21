7158d866753bdb60c9c6dfb7f787ceda9333dfff
Rob Fletcher <rfletcher@netflix.com>
2018-11-21 12:51:15 -0800

    Amend JAVA_HOME for Zulu

--- dev/path.zsh
+++ dev/path.zsh
@@ -4,6 +4,6 @@ export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 export GOPATH=~/Projects/go
 export PATH=$PATH:$GOPATH/bin
 
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export JAVA_HOME=`/usr/libexec/java_home -v 8`
 export GROOVY_HOME=/usr/local/opt/groovy/libexec
 export PATH=$GROOVY_HOME/bin:$PATH
