396b57c740bbfcae975d5311c4fc197c520d7bfd
Rob Fletcher <rfletcher@netflix.com>
2019-08-06 12:33:51 -0700

    p10k builtin configuration

--- zsh/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-( cd && curl -fsSLO https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.purepower )
-
-DIR=~/.oh-my-zsh/custom/themes/powerlevel10k
-if [[ ! -d $DIR ]]; then
-  mkdir -p `dirname $DIR`
-  ln -s ~/.powerlevel10k $DIR
-fi
