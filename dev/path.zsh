d7136a9aecf12f0df0feac6fb8f1044682c9560d
Rob Fletcher <rfletcher@netflix.com>
2020-09-23 07:52:32 -0700

    Java 11

--- dev/path.zsh
+++ dev/path.zsh
@@ -4,8 +4,8 @@ export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 export GOPATH=~/Projects/go
 export PATH=$PATH:$GOPATH/bin
 
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-export JDK_17=`/usr/libexec/java_home -v 1.7`
+export JAVA_HOME=`/usr/libexec/java_home -v 11`
+export JDK_8=`/usr/libexec/java_home -v 1.8`
 export GROOVY_HOME=/usr/local/opt/groovy/libexec
 export PATH=$GROOVY_HOME/bin:$PATH
 
